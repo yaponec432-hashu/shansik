f0e7353778682a1ab5a1c47e96a1e2e82a7d3b5f
yaponec432-hashu <234154007+yaponec432-hashu@users.noreply.github.com>
2026-04-09 10:42:55 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -4,9 +4,9 @@
 
 from asyncio import wait_for, TimeoutError
 from re import compile
-from os import environ
+from os import getenv
 from gpytranslate import Translator, TranslationError
-from simpleeval import simple_eval
+from discord.abc import Messageable
 from discord import (
     app_commands,
     Intents,
@@ -42,7 +42,7 @@ class GoidaBot(Client):
         }
 
     async def setup_hook(self) -> None:
-        if environ["BOT_SYNC_ENABLED"] == "1":
+        if getenv("BOT_SYNC_ENABLED") == "1":
             await self.tree.sync()
 
     async def on_message(self, message: Message) -> None:
@@ -51,12 +51,10 @@ class GoidaBot(Client):
         channel = message.channel
         channel_name = channel.name
         if not (
-            type(channel) is TextChannel
-            and not author.bot
-            and len(message_text) == self.sekai_code_len
-            and message_text.isdigit()
+            await is_human_in_text_channel(author, channel)
+            and await is_sekai_code(message_text)
             and (channel_match := self.channel_name_regex.match(channel_name))
-            and any(role.name in self.manager_roles for role in author.roles)
+            and await is_manager(author)
         ):
             return
         prefix = channel_match.group(1)
@@ -83,11 +81,6 @@ async def translate_into_english(ctx: Interaction, message: Message) -> None:
 async def translate_into_russian(ctx: Interaction, message: Message) -> None:
     await ctx.response.defer(ephemeral=True)
     result = await translate(message.content, "ru")
-    await reply(ctx, result, True)
-
-@bot.tree.context_menu(name="Посчитать")
-async def context_calculator(ctx: Interaction, message: Message) -> None:
-    result = await calculate(message.content)
     await reply(ctx, result, True)
 
 @bot.tree.command(description="Посчитать значение чтобы сравнить ISV")
@@ -124,12 +117,6 @@ async def member_avatar(ctx: Interaction, member: Member) -> None:
     result = member.display_avatar
     await reply(ctx, result)
 
-@bot.tree.command(description="Калькулятор")
-@app_commands.describe(expression="Напиши выражение, типа 2+2")
-async def calculator(ctx: Interaction, expression: str) -> None:
-    result = await calculate(expression)
-    await reply(ctx, result)
-
 @bot.tree.command(description="Посчитать длину строки")
 @app_commands.describe(text="Пиши свою строку")
 async def length(ctx: Interaction, text: str) -> None:
@@ -141,13 +128,6 @@ async def check_bot(ctx: Interaction) -> None:
     result = "Гойда"
     await reply(ctx, result)
 
-async def calculate(expression: str) -> str:
-    try:
-        result = simple_eval(expression.replace(",", ""))
-    except Exception:
-        result = "По понятиям пиши, вотак вот: 1*4/8**8 + (5-2)"
-    return result
-
 async def translate(source_text: str, target_language: str) -> str:
     translator = Translator()
     try:
@@ -157,6 +137,21 @@ async def translate(source_text: str, target_language: str) -> str:
         result = translation.text
     except TranslationError:
         result = "*Translation error, try again*"
+    return result
+
+async def is_human_in_text_channel(
+    author: Member,
+    channel: Messageable
+) -> bool:
+    result = not author.bot and type(channel) is TextChannel
+    return result
+
+async def is_sekai_code(text: str) -> bool:
+    result = len(text) == bot.sekai_code_len and text.isdigit()
+    return result
+
+async def is_manager(author: Member) -> bool:
+    result = any(role.name in bot.manager_roles for role in author.roles)
     return result
 
 async def reply(
@@ -174,4 +169,4 @@ async def reply(
         await ctx.response.send_message(result, silent=silent)
 
 if __name__ == "__main__":
-    bot.run(environ["BOT_TOKEN"])
+    bot.run(getenv("BOT_TOKEN"))
