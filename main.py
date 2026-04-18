2ccd931af0622a3a9344b3c491b2543abb1f1b15
yaponec432-hashu <234154007+yaponec432-hashu@users.noreply.github.com>
2026-04-18 20:47:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -72,9 +72,16 @@ class GoidaBot(AutoShardedClient):
 bot = GoidaBot()
 
 @bot.tree.context_menu(name="Перевести с Кристальского")
-async def translate_from_crystalian(ctx: Interaction, message: Message) -> None:
-    qwerty = "qwertyuiop[]asdfghjkl;'zxcvbnm,./QWERTYUIOP{}ASDFGHJKL:\"ZXCVBNM<>?"
-    russian = "йцукенгшщзхъфывапролджэячсмитьбю.ЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ,"
+async def translate_from_crystalian(
+    ctx: Interaction,
+    message: Message
+) -> None:
+    qwerty = (
+        "qwertyuiop[]asdfghjkl;'zxcvbnm,./QWERTYUIOP{}ASDFGHJKL:\"ZXCVBNM<>?"
+    )
+    russian = (
+        "йцукенгшщзхъфывапролджэячсмитьбю.ЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ,"
+    )
     table = str.maketrans(qwerty, russian)
     result = message.content.translate(table)
     await reply(ctx, result)
